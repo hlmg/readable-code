d7239feefb8a2bb46bd76bf8f5c72dca5819babe
hlmg <devhlmg@gmail.com>
2024-10-11 09:57:10 +0900

    객체에 메세지 보내기

--- src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
+++ src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
@@ -18,12 +18,17 @@ public class StudyCafePass {
         return new StudyCafePass(passType, duration, price, discountRate);
     }
 
-    public StudyCafePassType getPassType() {
-        return passType;
+    public boolean isSamePassType(StudyCafePassType passType) {
+        return this.passType == passType;
     }
 
-    public int getDuration() {
-        return duration;
+    public boolean isSameDurationType(StudyCafeLockerPass lockerPass) {
+        return lockerPass.isSamePassType(this.passType)
+            && lockerPass.isSameDuration(this.duration);
+    }
+
+    public boolean cannotUseLocker() {
+        return passType.isNotLockerType();
     }
 
     public int getPrice() {
